976dc733eaa3d36d13cefb213e04fd2184f28b55
jackxu <779868946@qq.com>
2021-03-20 21:29:49 +0800

    compont

--- src/main/java/com/jackxu/rocketmq/consumer/Consumer.java
+++ src/main/java/com/jackxu/rocketmq/consumer/Consumer.java
@@ -5,10 +5,12 @@ import org.apache.rocketmq.spring.annotation.MessageModel;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.annotation.SelectorType;
 import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.springframework.stereotype.Component;
 
 /**
  * @author jackxu
  */
+@Component
 @RocketMQMessageListener(topic = "springboot-topic", consumerGroup = "springboot-consumer-group",
         selectorExpression = "tag1", selectorType = SelectorType.TAG,
         messageModel = MessageModel.CLUSTERING, consumeMode = ConsumeMode.CONCURRENTLY)
